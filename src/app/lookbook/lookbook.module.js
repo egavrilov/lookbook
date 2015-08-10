7c4081eaa762cef9bed5242dc27bdad5b530ebf3
huncode <me@huncode.com>
2015-08-10 17:53:53 +0300

    Add ngResource dependence.

--- src/app/lookbook/lookbook.module.js
+++ src/app/lookbook/lookbook.module.js
@@ -1,11 +1,11 @@
-import LookbookFactory from './lookbook.factory.js';
-import Lookbook from './lookbook.directive.js';
+import LookbookFactory from './lookbook.factory';
+import Lookbook from './lookbook.directive';
 import RubleFilter from '../components/ruble.filter';
 import ScreenHeight from '../components/screenHeight.directive';
 import ResourceLoad from '../components/slLoad.directive';
 
 angular.module('lookbook', ['ngResource'])
-  .factory('Lookbook', ['$http', '$q', ($http, $q) => new LookbookFactory($http, $q)])
+  .factory('Lookbook', ['$http', ($http) => new LookbookFactory($http)])
   .directive('screenHeight', () => new ScreenHeight())
   .directive('slLoad', () => new ResourceLoad())
   .directive('lookbook', Lookbook)
